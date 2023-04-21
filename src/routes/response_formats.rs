9b3d1bba630b7a6cf1a5bd70bed273c0668c02d6
Kevin Swiber <kswiber@gmail.com>
2023-04-21 13:37:43 -0700

    Adding tests for root, request introspection, response formats, status codes, and server.

--- src/routes/response_formats.rs
+++ src/routes/response_formats.rs
@@ -1,6 +1,6 @@
 use axum::{response::Html, routing::get, Router};
 
-const UTF8_PAGE: &'static str = include_str!("../templates/utf8.html");
+const UTF8_PAGE: &str = include_str!("../templates/utf8.html");
 
 pub fn routes() -> Router {
     Router::new().route("/encoding/utf8", get(utf8))
@@ -8,4 +8,38 @@ pub fn routes() -> Router {
 
 async fn utf8() -> Html<&'static str> {
     UTF8_PAGE.into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{header, HeaderValue, Request, StatusCode},
+    };
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn utf8() {
+        let app = routes();
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/encoding/utf8")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(
+            response.headers().get(header::CONTENT_TYPE),
+            Some(&HeaderValue::from_static(mime::TEXT_HTML_UTF_8.as_ref()))
+        );
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert!(std::str::from_utf8(&body).is_ok())
+    }
 }
